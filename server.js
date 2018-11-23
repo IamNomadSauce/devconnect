0b8c4aa5d9bd69ee5834980c8efd393461bf2fc5
mgarsteck <garsteckphotography@gmail.com>
2018-11-23 10:07:20 -0800

    Routes

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const app = express();
 
 app.get('/', (req, res) => res.send('hello there'));
 
+// ROUTES
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
-const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
 
 // MONGO STUFF
 var db = require('./config/keys').mongoURI;
@@ -15,6 +16,12 @@ mongoose
   .then(() => console.log('MONGODB CONNECTED'))
   .catch(err => console.log(err));
 
+// USE ROUTES
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+// SERVER STUFF
 const port  = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`SERVER RUNNING ON PORT ${port}`));
